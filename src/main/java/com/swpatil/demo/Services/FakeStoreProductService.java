cd32acffc2b46b181db0df41d8982b7e39029342
swapnil <sp1791220@gmail.com>
2024-02-18 19:06:07 +0530

    To connect out API to FakeStore API we need RestTemplate.

--- src/main/java/com/swpatil/demo/Services/FakeStoreProductService.java
+++ src/main/java/com/swpatil/demo/Services/FakeStoreProductService.java
@@ -1,15 +1,24 @@
 package com.swpatil.demo.Services;
 
 import com.swpatil.demo.Dtos.GenericProductDto;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Primary
 @Service("FakeStoreProductService")
 public class FakeStoreProductService implements ProductService{
+    RestTemplateBuilder restTemplateBuilder;
+
+    public FakeStoreProductService(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
     @Override
     public GenericProductDto getProductById(Long id) {
         System.out.println("Call from FakeStore");
+        RestTemplate restTemplate = restTemplateBuilder.build();
         return null;
     }
 }
