4d8bea46b1ec109e001a10cb620173e0751fffac
swapnil <sp1791220@gmail.com>
2024-02-18 19:37:04 +0530

    FakeStore return extra information also for that we use ResponseEntity.

--- src/main/java/com/swpatil/demo/Services/FakeStoreProductService.java
+++ src/main/java/com/swpatil/demo/Services/FakeStoreProductService.java
@@ -4,6 +4,7 @@ import com.swpatil.demo.Dtos.FakeStoreProductDto;
 import com.swpatil.demo.Dtos.GenericProductDto;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,10 +21,10 @@ public class FakeStoreProductService implements ProductService{
     public GenericProductDto getProductById(Long id) {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        restTemplate.getForEntity(
-                "https://fakestoreapi.com/products/{id}",
-                FakeStoreProductDto.class,
-                id);
+        ResponseEntity<FakeStoreProductDto> responseEntity = restTemplate.getForEntity(
+                        "https://fakestoreapi.com/products/{id}",
+                        FakeStoreProductDto.class,
+                        id);
         return null;
     }
 }
