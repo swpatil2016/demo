c5277e2cec9e623bfb9ef3ccdc93d22c57019981
swapnil <sp1791220@gmail.com>
2024-02-18 17:34:28 +0530

    if hit url on postMan of gpbId method will get error because the dependency not injected in productService, it is null for now.

--- src/main/java/com/swpatil/demo/Controllers/ProductController.java
+++ src/main/java/com/swpatil/demo/Controllers/ProductController.java
@@ -1,10 +1,14 @@
 package com.swpatil.demo.Controllers;
 
+import com.swpatil.demo.Services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    ProductService productService;
 
     @GetMapping()
     public void getAllProducts(){
@@ -13,7 +17,7 @@ public class ProductController {
 
     @GetMapping("{id}")
     public void getProductById(@PathVariable("id") Long id){
-
+        productService.getProductById(id);
     }
 
     @PutMapping("{id}")
--- src/main/java/com/swpatil/demo/Services/FakeStoreProductService.java
+++ src/main/java/com/swpatil/demo/Services/FakeStoreProductService.java
@@ -1,2 +1,12 @@
-package com.swpatil.demo.Services;public class FakeStoreProductService {
+package com.swpatil.demo.Services;
+
+import com.swpatil.demo.Dtos.GenericProductDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FakeStoreProductService implements ProductService{
+    @Override
+    public GenericProductDto getProductById(Long id) {
+        return null;
+    }
 }
