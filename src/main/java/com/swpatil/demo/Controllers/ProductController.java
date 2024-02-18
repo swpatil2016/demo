2849d5d1493958d4609345952ad07d1dd43e4330
swapnil <sp1791220@gmail.com>
2024-02-18 15:35:45 +0530

    Using this annotation client interact with our API. Like @RestController, @Get etc.

--- src/main/java/com/swpatil/demo/Controllers/ProductController.java
+++ src/main/java/com/swpatil/demo/Controllers/ProductController.java
@@ -1,20 +1,29 @@
 package com.swpatil.demo.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
 
-
+    @GetMapping("/products")
     public void getAllProducts(){
 
     }
 
+    @GetMapping("/products/{id}")
     public void getProductById(){
 
     }
 
+    @PutMapping("/products/{id}")
     public void updateProductById(){
 
     }
 
+    @PostMapping("/products")
     public void createProduct(){
 
     }
