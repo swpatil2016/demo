44897590cbe53e9014c9ab9172c4d666dd96e576
swapnil <sp1791220@gmail.com>
2024-02-18 10:41:50 +0530

    Take more than one input by @Requestparam

--- src/main/java/com/swpatil/demo/TestController.java
+++ src/main/java/com/swpatil/demo/TestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
     private int id;
+    private String name;
 
     @GetMapping("/hwllo")
     public String printString(){
@@ -22,8 +23,23 @@ public class TestController {
         return "This is id: " + id;
     }
 
-    @GetMapping("/number")
-    public int printNumber(@RequestParam(value = "number", defaultValue = "10") int id){
+    @GetMapping("/value")
+    public int printNumber(@RequestParam(value = "number", defaultValue = "10000") int id){
         return id;
     }
+
+    /*
+        Pass more than one value;
+        Order does not matter;
+        http://localhost:8080/std?name=swapnil&id=200
+        http://localhost:8080/std?id=100&name=swapnil
+
+    * */
+    @GetMapping("/std")
+    public String studentDetail(
+            @RequestParam(value = "id", defaultValue = "10") int id,
+            @RequestParam(value = "name", defaultValue = "student") String name) {
+        return "This is Student name: " + name + " and this is his id: " + id + ".";
+    }
+
 }
