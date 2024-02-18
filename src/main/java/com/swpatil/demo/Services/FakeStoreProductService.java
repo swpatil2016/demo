03c7441084ad84f8f99fa57732171e85344f099c
swapnil <sp1791220@gmail.com>
2024-02-18 18:12:03 +0530

    make one of the service default using @Primary.

--- src/main/java/com/swpatil/demo/Services/FakeStoreProductService.java
+++ src/main/java/com/swpatil/demo/Services/FakeStoreProductService.java
@@ -1,8 +1,10 @@
 package com.swpatil.demo.Services;
 
 import com.swpatil.demo.Dtos.GenericProductDto;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+@Primary
 @Service("FakeStoreProductService")
 public class FakeStoreProductService implements ProductService{
     @Override
