c374deed8dce65ea15d8ec1feee65b70993cefd8
swapnil <sp1791220@gmail.com>
2024-02-22 14:46:32 +0530

    mappedBy comes before only @OneToMany.

--- src/main/java/com/swpatil/demo/Models/Category.java
+++ src/main/java/com/swpatil/demo/Models/Category.java
@@ -1,12 +1,18 @@
 package com.swpatil.demo.Models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Entity
 public class Category extends BaseModel {
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
